bb4d9e79d3f258fc2dbe287b4f36ed07910e3939
vinibailo <131203547+vinibailo@users.noreply.github.com>
2025-09-08 12:47:26 +0200

    Revert "feat: enable dark mode and cropper"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,35 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
-
-const theme = extendTheme({
-  config: {
-    initialColorMode: 'dark',
-    useSystemColorMode: false,
-  },
-  colors: {
-    brand: {
-      500: '#06AED5',
-    },
-    game: {
-      500: '#C44536',
-    },
-  },
-  styles: {
-    global: {
-      body: {
-        bg: '#222222',
-        color: '#D4D2D5',
-      },
-    },
-  },
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
